c3981999fee3310f3f732097e66445c97a28ea4f
stickler-ci <support@stickler-ci.com>
2019-10-16 20:29:17 +0000

    Fixing style errors.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context "Post" do
-    
-    it "Creates a post that belongs to only one user" do
-        @user = User.new username: "example", email: "example@user.com", password: "test123", password_confirmation: "test123"
-        @post = @user.posts.new content: 'example content'
-        expect(@post.valid?).to eql(true), "The post got to be created by a user"
+  context 'Post' do
+    it 'Creates a post that belongs to only one user' do
+      @user = User.new username: 'example', email: 'example@user.com', password: 'test123', password_confirmation: 'test123'
+      @post = @user.posts.new content: 'example content'
+      expect(@post.valid?).to eql(true), 'The post got to be created by a user'
     end
   end
 end
