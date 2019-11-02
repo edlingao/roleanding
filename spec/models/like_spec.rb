85ac8d602105e935560307fc0bef9d78f48ac6ab
edlingao <sonido244@gmail.com>
2019-11-02 10:52:00 -0600

    Added unit testing on like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,6 +2,25 @@
 
 require 'rails_helper'
 
+
+
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @user = User.new(username: 'example', email: 'example@user.com',
+    password: 'test123', password_confirmation: 'test123')
+    
+    @post = @user.posts.new content: 'example content'
+    @like = @post.likes.new user_id: @user.id
+  end
+  context "On liking a post" do
+    
+    it "Belongs to one post" do
+
+      expect(@like.post_id).to eql(@post.id)
+        
+    end
+    it "Like belongs to one user" do
+      expect(@like.user_id).to eql(@user.id)
+    end
+  end
 end
