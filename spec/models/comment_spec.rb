eedfa609f8dd41e629aa8bc469aff68a8fcdd748
stickler-ci <support@stickler-ci.com>
2019-11-02 17:01:50 +0000

    Fixing style errors.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  context  "On creating comments" do
+  context 'On creating comments' do
     before(:all) do
       @user = User.new(username: 'example', email: 'example@user.com',
-      password: 'test123', password_confirmation: 'test123')
-      
+                       password: 'test123', password_confirmation: 'test123')
+
       @post = @user.posts.new content: 'example content'
       @comment = @post.comments.new user_id: @user.id
-
     end
-    it "Belongs to a user" do 
+    it 'Belongs to a user' do
       expect(@comment.user_id).to eql(@user.id)
     end
 
-    it "Belongs to a post" do
+    it 'Belongs to a post' do
       expect(@comment.post_id).to eql(@post.id)
     end
   end
