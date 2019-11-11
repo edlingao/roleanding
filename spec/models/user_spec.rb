b35bb8f97ec9b4025c85dc49c34b6d0bbbe3b092
stickler-ci <support@stickler-ci.com>
2019-11-11 16:37:27 +0000

    Fixing style errors.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
@@ -8,19 +10,17 @@ RSpec.describe User, type: :model do
     @friendship.status = 'waiting'
     @friendship.save
   end
-  it "must have one friend" do
-    
-    expect(@friend.pending[0].id).to eql(@user.id) 
+  it 'must have one friend' do
+    expect(@friend.pending[0].id).to eql(@user.id)
   end
-  it "must accept a friendship" do
-    @friendship.status = "friends"
+  it 'must accept a friendship' do
+    @friendship.status = 'friends'
     @friendship.save
 
     expect(@user.has_friends?).to eql(true)
   end
-  it "must delete a friendship" do
+  it 'must delete a friendship' do
     @friendship.destroy
     expect(@user.has_friends?).to eql(false)
   end
-  
 end
